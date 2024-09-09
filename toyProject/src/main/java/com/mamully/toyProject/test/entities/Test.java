586f4536aff73e8c2f833ef9fc9e6e783e9e040d
kookong2 <hy010309@naver.com>
2024-09-09 11:13:40 +0900

    refactor test 코드 entity 수정

--- toyProject/src/main/java/com/mamully/toyProject/test/entities/Test.java
+++ toyProject/src/main/java/com/mamully/toyProject/test/entities/Test.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Test {
 
     @Id
@@ -17,6 +21,6 @@ public class Test {
     private String content;
 
     public Test(String content) {
-        this.content = content;
+        this.content = content;  // content 필드 초기화
     }
 }
