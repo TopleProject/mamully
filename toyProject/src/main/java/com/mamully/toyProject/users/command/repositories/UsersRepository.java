b755e25d00cb8ff2173829e7495197e3f900eb6a
leebackcoding <lcy2825@gmail.com>
2024-09-15 22:34:32 +0900

    feat UserRepository singUp 구현

---
+++ toyProject/src/main/java/com/mamully/toyProject/users/command/repositories/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.mamully.toyProject.users.command.repositories;
+
+import com.mamully.toyProject.users.command.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+    boolean existsByUserId(String userId);
+    boolean existsByUserPhone(String userPhone);
+    boolean existsByUserEmail(String userEmail);
+}
