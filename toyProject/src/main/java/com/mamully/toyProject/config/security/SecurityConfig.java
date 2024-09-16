8b0b3326626be4db461ef999ae518caa0ff97394
leebackcoding <lcy2825@gmail.com>
2024-09-16 15:56:18 +0900

    feat Login SecurityConfig 설정 추가, 빈 통합

---
+++ toyProject/src/main/java/com/mamully/toyProject/config/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.mamully.toyProject.config.security;
+
+import com.mamully.toyProject.users.command.services.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+@RequiredArgsConstructor
+public class SecurityConfig  {
+
+    private final CustomUserDetailService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/api/v1/login",
+                            "/",
+                            "/swagger-ui/**",
+                            "/swagger-resources/**",
+                            "/v3/api-docs/**"
+                    ).permitAll()// "/login" 경로는 인증 없이 접근 허용
+                    .anyRequest().authenticated() // 그 외의 요청은 인증 필요
+
+            )
+                .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
+        );
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+
+}
