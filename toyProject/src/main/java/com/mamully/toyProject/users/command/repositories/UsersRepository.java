4f14eea00871237399f4830d83e4d1f2a1ab803a
leebackcoding <lcy2825@gmail.com>
2024-09-16 16:10:12 +0900

    feat User를 조회 메서드 추가

--- toyProject/src/main/java/com/mamully/toyProject/users/command/repositories/UsersRepository.java
+++ toyProject/src/main/java/com/mamully/toyProject/users/command/repositories/UsersRepository.java
@@ -3,8 +3,12 @@ package com.mamully.toyProject.users.command.repositories;
 import com.mamully.toyProject.users.command.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<User, Long> {
     boolean existsByUserId(String userId);
     boolean existsByUserPhone(String userPhone);
     boolean existsByUserEmail(String userEmail);
+
+    Optional<User> findByUserId(String username);
 }
